80291ecd4c7ce0f7aa6a5df9d9ad4b5f07a1437e
Collin <candrew022@gmail.com>
2016-12-20 22:28:56 -0500

    change export type

--- src/schema/schemaHelpers.js
+++ src/schema/schemaHelpers.js
@@ -5,16 +5,13 @@ const getUserById = (id) => {
   return User.findByIdAsync(id)
 }
 
-const userLoader = new DataLoader(
+export const userLoader = new DataLoader(
   keys => Promise.all(keys.map(getUserById)),
   {
-    cacheKeyFn: key => {
-      return key.toString();
-    }
+    cacheKeyFn: key => key.toString()
   }
 )
 
-export default userLoader;
 
 export const getObjectById = (type, id) => {
 
