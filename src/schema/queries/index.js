bb60900ec0e94da0bd7db762fe18a6b9f76e6589
Collin <candrew022@gmail.com>
2016-12-22 20:38:55 -0500

    extract queries from schema index

---
+++ src/schema/queries/index.js
@@ -0,0 +1,60 @@
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLID
+} from 'graphql';
+import {
+  connectionFromPromisedArray,
+  connectionArgs
+} from 'graphql-relay';
+import {
+  getObjectsByType,
+  getObjectById
+} from '../schemaHelpers.js';
+import UserType, { UserConnection } from '../types/User';
+import OrganizationType, { OrganizationConnection } from '../types/Organization';
+import MatchType, { MatchConnection } from '../types/Match';
+import LeagueType, { LeagueConnection } from '../types/League';
+import EventType, { EventConnection } from '../types/Event';
+import { nodeField } from '../node';
+
+const rootField = (GQLType, type) => {
+  return {
+    type: GQLType,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID),
+        description: `ID of a ${type}`
+      }
+    },
+    resolve: (_, args) => getObjectById(type, args.id)
+  }
+}
+
+const rootConnection = (connection, type) => {
+  return {
+    type: connection,
+    args: connectionArgs,
+    resolve: (_, args) => connectionFromPromisedArray(
+      getObjectsByType(type),
+      args
+    )
+  }
+}
+
+export const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    node: nodeField,
+    user: rootField(UserType, 'user'),
+    users: rootConnection(UserConnection, 'user'),
+    organization: rootField(OrganizationType, 'organization'),
+    organizations: rootConnection(OrganizationConnection, 'organization'),
+    match: rootField(MatchType, 'match'),
+    matches: rootConnection(MatchConnection, 'match'),
+    league:rootField(LeagueType, 'league'),
+    leagues: rootConnection(LeagueConnection, 'league'),
+    event: rootField(EventType, 'event'),
+    events: rootConnection(EventConnection, 'event')
+  })
+})
