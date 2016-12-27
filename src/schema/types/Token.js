b6e91469eb069bc1f4ab5d95d2b5f26b6450fd1a
Collin <candrew022@gmail.com>
2016-12-27 18:19:39 -0500

    resolve org information

--- src/schema/types/Token.js
+++ src/schema/types/Token.js
@@ -2,10 +2,16 @@ import {
   GraphQLObjectType,
   GraphQLString
 } from 'graphql';
+import { organizationLoader } from '../schemaHelpers';
+import OrganizationType from './Organization';
 
 const TokenType = new GraphQLObjectType({
   name: 'Token',
   fields: () => ({
+    orgId: {
+      type: OrganizationType,
+      resolve: (token) => organizationLoader.load(token.orgId)
+    },
     access_token: {
       type: GraphQLString
     },
