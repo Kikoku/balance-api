f3575aebab7ddfe071fc3db9c0a1c950534f333a
Collin <candrew022@gmail.com>
2016-12-27 18:11:12 -0500

    resolve roles on organization type

--- src/schema/types/Organization.js
+++ src/schema/types/Organization.js
@@ -1,13 +1,16 @@
 import {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } from 'graphql';
 import {
   globalIdField,
   connectionDefinitions
 } from 'graphql-relay'
 import { nodeInterface  } from '../node';
+import RoleType from './Role';
+import Role from '../../../models/types/Role';
 
 const OrganizationType = new GraphQLObjectType({
   name: 'Organization',
@@ -22,6 +25,10 @@ const OrganizationType = new GraphQLObjectType({
     password: {
       type: GraphQLString,
       resolve: () => null
+    },
+    roles: {
+      type: new GraphQLList(RoleType),
+      resolve: (org, args) => Role.findAsync({_id: { $in: org.roles}})
     }
   }),
   interfaces: () => [nodeInterface]
