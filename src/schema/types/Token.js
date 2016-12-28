5483bb1cefcae807dc5614d0e9fed6d0e29efac1
Collin <candrew022@gmail.com>
2016-12-27 20:05:42 -0500

    remove extra fields

--- src/schema/types/Token.js
+++ src/schema/types/Token.js
@@ -2,20 +2,11 @@ import {
   GraphQLObjectType,
   GraphQLString
 } from 'graphql';
-import { organizationLoader } from '../schemaHelpers';
-import OrganizationType from './Organization';
 
 const TokenType = new GraphQLObjectType({
   name: 'Token',
   fields: () => ({
-    orgId: {
-      type: OrganizationType,
-      resolve: (token) => organizationLoader.load(token.orgId)
-    },
     access_token: {
-      type: GraphQLString
-    },
-    error: {
       type: GraphQLString
     }
   })
