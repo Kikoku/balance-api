fb51904ee172cfe2fb80eadc38df5a564d2e7b10
Collin <candrew022@gmail.com>
2016-12-28 09:31:02 -0500

    remove newUser from mutation index

--- src/schema/mutations/index.js
+++ src/schema/mutations/index.js
@@ -7,7 +7,6 @@ import {
   mutationWithClientMutationId
 } from 'graphql-relay';
 import { newOrganization } from './newOrganization';
-import { newUser } from './newUser';
 import { newLeague } from './newLeague';
 import { newEvent } from './newEvent';
 import { login } from './login';
@@ -16,7 +15,6 @@ export const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     newOrganization,
-    newUser,
     newLeague,
     newEvent,
     login
