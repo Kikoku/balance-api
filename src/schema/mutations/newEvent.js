bc0cb5fbf0e7d7d2e00e8482d17889209279fed1
Collin <candrew022@gmail.com>
2016-12-29 01:54:40 -0500

    extract methods

--- src/schema/mutations/newEvent.js
+++ src/schema/mutations/newEvent.js
@@ -6,6 +6,7 @@ import {
 import {
   mutationWithClientMutationId
 } from 'graphql-relay';
+import Promise from 'bluebird';
 import EventType from '../types/Event';
 import Event from '../../../models/types/Event';
 import EventToOrg from '../../../models/relationships/EventToOrg';
@@ -14,6 +15,12 @@ import EventInputType from '../inputs/EventInput';
 import LogInputType from '../inputs/LogInput';
 import PlayerInputType from '../inputs/PlayerInput';
 import MatchInputType from '../inputs/MatchInput';
+import {
+  getLeaguePlayerInfo,
+  setMatches
+ } from './helpers';
+
+import User from '../../../models/types/User';
 
 export const newEvent = mutationWithClientMutationId({
   name: 'newEvent',
@@ -44,24 +51,25 @@ export const newEvent = mutationWithClientMutationId({
       resolve: ({error}) => error
     }
   },
-  mutateAndGetPayload: ({event, leagueId, logs, players, matches}, context ) => {
+  mutateAndGetPayload: async ({event, leagueId, logs, players, matches}, context ) => {
 
     if(context.viewer) {
       let newEvent = new Event(event)
-      return newEvent.saveAsync()
-      .then( event => {
-        let newEventToOrg = new EventToOrg({
-          eventId: event.id,
-          orgId: context.viewer._id
-        })
-        newEventToOrg.saveAsync();
-        let newLeagueToEvent = new LeagueToEvent({
-          eventId: event.id,
-          leagueId
-        })
-        newLeagueToEvent.saveAsync();
-        return {event};
-      });
+      event = await newEvent.saveAsync();
+      players = await getLeaguePlayerInfo(players, leagueId);
+      players = await setMatches(players, matches, event.id, leagueId);
+
+      let newEventToOrg = new EventToOrg({
+        eventId: event.id,
+        orgId: context.viewer._id
+      })
+      newEventToOrg.saveAsync();
+      let newLeagueToEvent = new LeagueToEvent({
+        eventId: event.id,
+        leagueId
+      })
+      newLeagueToEvent.saveAsync();
+      return {event};
     } else {
       return {
         error: 'Your account is not authorized to perform this action'
