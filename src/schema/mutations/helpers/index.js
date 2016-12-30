b7ecaef478d4ffd9439075c5afa0ddace5038556
Collin <candrew022@gmail.com>
2016-12-30 12:21:19 -0500

    export save functions to helper file

--- src/schema/mutations/helpers/index.js
+++ src/schema/mutations/helpers/index.js
@@ -1,7 +1,19 @@
 import getLeaguePlayerInfo from './getLeaguePlayerInfo';
 import calcEvent from './calcEvent';
+import {
+  saveLogs,
+  saveMatches,
+  savePlayers,
+  saveLeague,
+  saveOrg
+} from './calcEventSave';
 
 export {
   getLeaguePlayerInfo,
-  calcEvent
+  calcEvent,
+  saveLogs,
+  saveMatches,
+  savePlayers,
+  saveLeague,
+  saveOrg
 }
--- src/schema/mutations/newEvent.js
+++ src/schema/mutations/newEvent.js
@@ -14,19 +14,14 @@ import PlayerInputType from '../inputs/PlayerInput';
 import MatchInputType from '../inputs/MatchInput';
 import {
   getLeaguePlayerInfo,
-  calcEvent
+  calcEvent,
+  saveLogs,
+  saveMatches,
+  savePlayers,
+  saveLeague,
+  saveOrg
  } from './helpers';
-import User from '../../../models/types/User';
-import Match from '../../../models/types/Match';
 import Event from '../../../models/types/Event';
-import Log from '../../../models/types/Log';
-import EventToMatch from '../../../models/relationships/EventToMatch';
-import MatchToUser from '../../../models/relationships/MatchToUser';
-import EventToUser from '../../../models/relationships/EventToUser';
-import LeagueToUser from '../../../models/relationships/LeagueToUser';
-import LeagueToEvent from '../../../models/relationships/LeagueToEvent';
-import EventToOrg from '../../../models/relationships/EventToOrg';
-import EventToLog from '../../../models/relationships/EventToLog';
 
 export const newEvent = mutationWithClientMutationId({
   name: 'newEvent',
@@ -112,91 +107,18 @@ export const newEvent = mutationWithClientMutationId({
         status,
         title
       })
-      return newEvent.saveAsync()
-      .then(savedEvent => {
 
-        event.logs.map(log => {
-          let newLog = new Log(log)
-          newLog.saveAsync()
-          .then(savedLog => {
-            let newEventToLog = new EventToLog({
-              logId: savedLog.id,
-              eventId: savedEvent.id
-            })
-          })
-        })
+      newEvent = await newEvent.saveAsync();
+      await saveLogs(event.logs, newEvent.id);
+      await saveMatches(event.matches, newEvent.id);
+      await savePlayers(event.players, newEvent.id, leagueId);
+      await saveLeague(leagueId, newEvent.id);
+      await saveOrg(context.viewer._id, newEvent.id);
 
-        event.matches.map(match => {
-          let newMatch = new Match({
-            round: match.round,
-            PlayFormat: match.PlayFormat,
-            date: match.date,
-            teamformat: match.teamformat
-          })
-          newMatch.saveAsync()
-          .then(savedMatch => {
-            let newEventToMatch = new EventToMatch({
-              matchId: savedMatch.id,
-              eventId: savedEvent.id
-            })
-            newEventToMatch.saveAsync();
+      return {
+        event: newEvent
+      }
 
-            let newMatchToUser = new MatchToUser({
-              person: match.p1Id,
-              opponent: match.p2Id,
-              matchId: savedMatch.id,
-              win: match.win,
-              loss: match.loss,
-              draw: match.draw,
-              p1elo: match.p1elo,
-              p1change: match.p1change,
-              p2elo: match.p2elo,
-              p2change: match.p2change,
-              outcome: match.outcome,
-              windbydrop: match.winbydrop
-            })
-            newMatchToUser.saveAsync();
-          })
-        })
-
-        event.players.map(player => {
-          let newEventToUser = new EventToUser({
-            userId: player.id,
-            eventId: savedEvent.id,
-            win: player.win,
-            loss: player.loss,
-            draw: player.draw,
-            elo: player.elo,
-            change: player.change
-          })
-          newEventToUser.saveAsync();
-
-          let newLeagueToUser = new LeagueToUser({
-            userId: player.id,
-            eventId: savedEvent.id,
-            win: player.win,
-            loss: player.loss,
-            draw: player.draw,
-            elo: player.elo,
-            change: player.change,
-            attendance: player.attendance
-          })
-          newLeagueToUser.saveAsync();
-        })
-
-        let newLeagueToEvent = new LeagueToEvent({
-          leagueId,
-          eventId: savedEvent.id
-        })
-        newLeagueToEvent.saveAsync();
-
-        let newEventToOrg = new EventToOrg({
-          eventId: savedEvent.id,
-          orgId: context.viewer._id
-        })
-        newEventToOrg.saveAsync()
-        return {event: savedEvent}
-      })
     } else {
       return {
         error: 'Your account is not authorized to perform this action'
