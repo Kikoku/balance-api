d0a4deb112169775a71046b15e43e6137eaeee1c
Collin <candrew022@gmail.com>
2017-01-04 22:30:39 -0500

    remove dotenv from production

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ import schema from './src/schema';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
-require('dotenv').config();
+
+let app = express();
+
+if (app.get('env') === 'development') {
+  require('dotenv').config();
+}
 
 mongoose.connect(process.env.MONGODB_URI, (err) => {
   if(err) console.error(err)
   else console.log('Mongodb connected')
 })
 
-let app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use('/', cors(), graphQLHTTP( req => {
