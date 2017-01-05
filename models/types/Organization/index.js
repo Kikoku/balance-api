f34569ac99f2b6d864e5ed7c90a434cf28a01ea2
Collin <candrew022@gmail.com>
2017-01-04 22:13:54 -0500

    add password methods

--- models/types/Organization/index.js
+++ models/types/Organization/index.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import Bluebird from 'bluebird';
+import bcrypt from 'bcrypt';
 
 const organizationSchema = new Schema ({
   name: String,
@@ -10,6 +11,49 @@ const organizationSchema = new Schema ({
     ref: 'Role'
   }]
 });
+
+organizationSchema.pre('save', function(next) {
+  let org = this;
+  if(!org.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(5, (err, salt) => {
+    if(err) {
+      return next(err);
+    }
+    bcrypt.hash(org.password, salt, (err, hash) => {
+      if(err) {
+        return next(err);
+      }
+      org.password = hash;
+      next();
+    })
+  })
+});
+
+organizationSchema.methods = {
+  comparePassword(pw, cb) {
+
+    if (!pw) {
+      return cb({
+        msg: 'No Password Supplied'
+      });
+    }
+
+    bcrypt.compare(pw, this.password, function(err, isMatch) {
+      if(err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  },
+  toJSON() {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+  }
+}
 
 const Organization = mongoose.model('Organization', organizationSchema);
 
