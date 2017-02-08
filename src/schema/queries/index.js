fffeeb1aca1ebb9b647ec219a6bab027c358b39c
Collin <candrew022@gmail.com>
2017-02-08 13:50:55 -0500

    Move Queires under ViewerType

--- src/schema/queries/index.js
+++ src/schema/queries/index.js
@@ -11,37 +11,8 @@ import {
   getObjectsByType,
   getObjectById
 } from '../schemaHelpers.js';
-import UserType, { UserConnection } from '../types/User';
-import OrganizationType, { OrganizationConnection } from '../types/Organization';
-import MatchType, { MatchConnection } from '../types/Match';
-import LeagueType, { LeagueConnection } from '../types/League';
-import EventType, { EventConnection } from '../types/Event';
 import ViewerType from '../types/Viewer';
 import { nodeField } from '../node';
-
-const rootField = (GQLType, type) => {
-  return {
-    type: GQLType,
-    args: {
-      id: {
-        type: new GraphQLNonNull(GraphQLID),
-        description: `ID of a ${type}`
-      }
-    },
-    resolve: (_, args) => getObjectById(type, args.id)
-  }
-}
-
-const rootConnection = (connection, type) => {
-  return {
-    type: connection,
-    args: connectionArgs,
-    resolve: (_, args) => connectionFromPromisedArray(
-      getObjectsByType(type),
-      args
-    )
-  }
-}
 
 export const queryType = new GraphQLObjectType({
   name: 'Query',
@@ -50,16 +21,6 @@ export const queryType = new GraphQLObjectType({
     viewer: {
       type: ViewerType,
       resolve: (_, args, { viewer } ) => viewer
-    },
-    user: rootField(UserType, 'user'),
-    users: rootConnection(UserConnection, 'user'),
-    organization: rootField(OrganizationType, 'organization'),
-    organizations: rootConnection(OrganizationConnection, 'organization'),
-    match: rootField(MatchType, 'match'),
-    matches: rootConnection(MatchConnection, 'match'),
-    league:rootField(LeagueType, 'league'),
-    leagues: rootConnection(LeagueConnection, 'league'),
-    event: rootField(EventType, 'event'),
-    events: rootConnection(EventConnection, 'event')
+    }
   })
 })
--- src/schema/types/Viewer.js
+++ src/schema/types/Viewer.js
@@ -2,14 +2,51 @@ import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLID
 } from 'graphql';
 import {
   globalIdField,
-  connectionDefinitions
-} from 'graphql-relay'
+  connectionDefinitions,
+  connectionArgs,
+  connectionFromPromisedArray
+} from 'graphql-relay';
+import {
+  getObjectsByType,
+  getObjectById
+} from '../schemaHelpers.js';
+import UserType, { UserConnection } from './User';
+import OrganizationType, { OrganizationConnection } from './Organization';
+import MatchType, { MatchConnection } from './Match';
+import LeagueType, { LeagueConnection } from './League';
+import EventType, { EventConnection } from './Event';
 import RoleType from './Role';
 import Role from '../../../models/types/Role';
+
+const rootField = (GQLType, type) => {
+  return {
+    type: GQLType,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID),
+        description: `ID of a ${type}`
+      }
+    },
+    resolve: (_, args) => getObjectById(type, args.id)
+  }
+}
+
+const rootConnection = (connection, type) => {
+  return {
+    type: connection,
+    args: connectionArgs,
+    resolve: (_, args) => connectionFromPromisedArray(
+      getObjectsByType(type),
+      args
+    )
+  }
+}
 
 const ViewerType = new GraphQLObjectType({
   name: 'Viewer',
@@ -20,7 +57,17 @@ const ViewerType = new GraphQLObjectType({
     roles: {
       type: new GraphQLList(RoleType),
       resolve: (viewer, args) => viewer.roles.length > 1 ? Role.findAsync({_id: { $in: viewer.roles}}) : [{name: 'guest'}]
-    }
+    },
+    user: rootField(UserType, 'user'),
+    users: rootConnection(UserConnection, 'user'),
+    organization: rootField(OrganizationType, 'organization'),
+    organizations: rootConnection(OrganizationConnection, 'organization'),
+    match: rootField(MatchType, 'match'),
+    matches: rootConnection(MatchConnection, 'match'),
+    league:rootField(LeagueType, 'league'),
+    leagues: rootConnection(LeagueConnection, 'league'),
+    event: rootField(EventType, 'event'),
+    events: rootConnection(EventConnection, 'event')
   })
 });
 
