669af479f3385395a8560bc6dce18366d7b7a463
Collin <candrew022@gmail.com>
2017-03-24 15:42:45 -0400

    Create SearchType

---
+++ src/schema/types/Search.js
@@ -0,0 +1,58 @@
+import {
+  GraphQLInterfaceType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt
+} from 'graphql';
+import EventType from './Event';
+import LeagueType from './League';
+import OrganizationType from './Organization';
+import UserType from './User';
+import MatchType from './Match';
+import {
+  connectionDefinitions,
+} from 'graphql-relay';
+
+
+const SearchType = new GraphQLInterfaceType({
+  name: 'Search',
+  fields: {
+    id: {
+      type: GraphQLID
+    },
+    createdDate: {
+      type: GraphQLString
+    }
+  },
+  description: 'Search for things',
+  resolveType: (data) => {
+    if(data.dci) {
+      return UserType
+    }
+    if(data.roles) {
+      return OrganizationType
+    }
+    if(data.sanctionnumber) {
+      return EventType
+    }
+    if(data.title && !data.sanctionnumber) {
+      return LeagueType
+    }
+    if(data.rounds) {
+      return MatchType
+    }
+  }
+})
+
+export const { connectionType: SearchConnection } = connectionDefinitions({
+  nodeType: SearchType,
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+      description: 'A count of the total number of items in this connection.',
+      resolve: (conn) => conn.edges.length
+    }
+  })
+})
+
+export default SearchType;
