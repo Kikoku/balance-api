1ed0513d582228b4f531aa345d632232eca060d0
Collin <candrew022@gmail.com>
2017-03-30 14:17:41 -0400

    Add org update to mutations list

--- src/schema/mutations/index.js
+++ src/schema/mutations/index.js
@@ -7,15 +7,17 @@ import {
   mutationWithClientMutationId
 } from 'graphql-relay';
 import { newOrganization } from './newOrganization';
+import { updateOrganization } from './updateOrganization';
 import { newLeague } from './newLeague';
 import { newEvent } from './newEvent';
 import { login } from './login';
-import { logout } from './logout'
+import { logout } from './logout';
 
 export const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     newOrganization,
+    updateOrganization,
     newLeague,
     newEvent,
     login,
